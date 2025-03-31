27d8d80a53a198ed566d8b9e5da77929a04df909
Harsh Shukla <shuklaharsh322@gmail.com>
2025-04-01 00:24:13 +0530

    initial commit

---
+++ src/sections/Download.jsx
@@ -0,0 +1,92 @@
+import { Element } from "react-scroll";
+import { links, logos } from "../constants/index.jsx";
+import { Marker } from "../components/Marker.jsx";
+
+const Download = () => {
+  return (
+    <section>
+      <Element
+        name="download"
+        // below class names are for making foundation of our download section like gradiesnt and space visibility
+        className="g7 relative pb-32 pt-24 max-lg:pb-24 max-md:py-16"
+      >
+        <div className="container">
+          <div className="flex items-center">
+            <div className="relative mr-6 flex-540 max-xl:flex-280 max-lg:flex256 max-md:flex-100">
+              <div className="mb-10">
+                <img
+                  src="/images/xora.svg"
+                  width={160}
+                  height={55}
+                  alt="xora"
+                />
+              </div>
+
+              <p className="body-1 mb-10 max-w-md">
+                Try it now for free on iOS, Android, PC, Web - whatever your
+                flavor, we've got you covered.
+              </p>
+
+              {/*icon for downloading from different apps*/}
+
+              <ul className="flex flex-wrap items-center gap-6">
+                {links.map(({ id, url, icon }) => (
+                  <li
+                    key={id}
+                    className="download_tech-link download_tech-link_last-before download_tech-link_last-after"
+                  >
+                    <a
+                      href={url}
+                      className="size-22 download_tech-icon_before relative flex items-center justify-center rounded-half border-2 border-s3 bg-s1 transition-borderColor duration-500"
+                    >
+                      <span className="absolute -top-2 rotate-90">
+                        <Marker />
+                      </span>
+                      <img
+                        // this image is a vector for wraping the icon of apps
+                        src={"/images/lines.svg"}
+                        alt="lines"
+                        className="absolute size-13/20 object-contain"
+                      />
+                      <span className="download_tech-icon">{icon}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="mb-10 max-md:hidden">
+              {/*this div for lines in the download section for that image interface*/}
+              <div className="download_preview-before download_preview-after rounded-40 relative w-[955px] border-2 border-s5 p-6">
+                {/*this div below is for creating the shape in which our screen image would be placed*/}
+                <div className="relative rounded-3xl bg-s1 px-6 pb-6 pt-14">
+                  {/*these spans for three dot near screen image like in macos something like we are making a  mockup of a screen*/}
+                  <span className="download_preview-dot left-6 bg-p2" />
+                  <span className="download_preview-dot left-11 bg-s3" />
+                  <span className="download_preview-dot left-16 bg-p1/15" />
+
+                  <img
+                    src="/images/screen.jpg"
+                    width={855}
+                    height={655}
+                    alt="screen"
+                    className="rounded-xl"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <ul className="mt-24 flex justify-center max-lg:hidden">
+            {logos.map(({ id, url, width, height, title }) => (
+              <li key={id} className="mx-10">
+                <img src={url} width={width} height={height} alt={title} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Element>
+    </section>
+  );
+};
+export default Download;
